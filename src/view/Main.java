6592fb356bb103a57f7424f4cbec33c8c371720b
Zenneo <agrar.pfurtz@gmail.com>
2016-05-30 12:13:07 +0200

    Fixed issues with .classpaths

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,5 +1,26 @@
 package view;
 
-public class Main {
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
+public class Main {
+	void main() {
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.open();
+
+		// run the event loop as long as the window is open
+		while (!shell.isDisposed()) {
+			// read the next OS event queue and transfer it to a SWT event
+			if (!display.readAndDispatch()) {
+				// if there are currently no other OS event to process
+				// sleep until the next OS event is available
+				display.sleep();
+			}
+		}
+
+		// disposes all associated windows and their components
+		display.dispose();
+
+	}
 }
