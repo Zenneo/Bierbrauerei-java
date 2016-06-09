f7985710f93740e8f8469ef9df7558faa36f6c32
chunkleader <chunkleader@gmail.com>
2016-06-09 10:47:57 +0200

    added MainWindow (the one with main-method), removed window.java

---
+++ src/view/MainWindow.java
@@ -0,0 +1,55 @@
+package view;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.SWT;
+
+public class MainWindow {
+
+	protected Shell shell;
+	private Text txtSdf;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			MainWindow window = new MainWindow();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+		
+		txtSdf = new Text(shell, SWT.BORDER);
+		txtSdf.setText("Test");
+		txtSdf.setBounds(0, 10, 76, 21);
+
+	}
+
+}
