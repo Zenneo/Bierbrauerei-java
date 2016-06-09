e4cdca31da543bf775585c2304bdc43d653eb337
chunkleader <chunkleader@gmail.com>
2016-06-09 11:04:07 +0200

    Implemented the Bank

--- src/model/Bank.java
+++ src/model/Bank.java
@@ -4,24 +4,30 @@ public class Bank {
 	// This class keeps track of the currently available capital. It also
 	// handles money transfers
 
-	// --variables--
 	private int capital;
-	
+
 	public Bank(int initalCapital) {
 		capital = initalCapital;
 	}
-	
+
 	public int getCapital() {
 		return capital;
 	}
 
 	public void payIn(int amount) {
-		// TODO not implemented
-		// TODO throws exception when failed
+		if (amount < 0) {
+			throw new IllegalArgumentException(
+					"Es kann kein negativer Betrag eingezahlt werden");
+		} else {
+			capital += amount;
+		}
 	}
-	
+
 	public void payOut(int amount) {
-		// TODO not implemented
-		// TODO throws exception when failed
+		if (capital < amount) {
+			throw new IllegalArgumentException("zu wenig Geld auf der Bank");
+		} else {
+			capital -= amount;
+		}
 	}
 }
