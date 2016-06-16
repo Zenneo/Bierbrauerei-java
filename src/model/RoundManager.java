6d64b5ee0aa668bd6d48911e477596e6ac6d9907
EragonBromsohn <Eragon.Bromson@gmx.de>
2016-06-16 10:40:57 +0200

    work on Roundmanager

--- src/model/RoundManager.java
+++ src/model/RoundManager.java
@@ -9,6 +9,8 @@ public class RoundManager {
     private Brewery brewery;
     private Upgrademanager upgrademanager;
     private Event event;
+    private int sellOrderPrice;
+    private int sellOrderBier;
 	public int getRound() {
 		return round;
 	}
@@ -18,20 +20,22 @@ public class RoundManager {
 		brewery = new Brewery(400, 50);
 		upgrademanager = new Upgrademanager(bank, brewery);
 		event = new Event();
+		sellOrderPrice = 0;
+		sellOrderBier = 0;
 	}
 	
 	// does every operation and finishes a round if everything is valid 
 	public void nextRound() {
 		// TODO not done yet
-		
-		// Produce beer
-		
+		brewery.produce();
+		bank.payOut(brewery.getRoundlyCosts());
+		sell();
 		
 		// increment round counter
 		round++;
 	}
 	public void purchaseUpgrade(int typ, int tier) {
-		//typ = 0 => storaqge
+		//typ = 0 => storage
 		//typ = 1 => output
 		if (typ == 0)
 		{
@@ -39,5 +43,37 @@ public class RoundManager {
 		} else if (typ == 1) {
 			upgrademanager.buyUpgradeOutput(tier);
 		}
+	}
+	public void placeSellOrder(int biermenge, int preis)
+	{
+		if (sellOrderBier == 0)
+		{
+			sellOrderBier = biermenge;
+			sellOrderPrice = preis;
+			brewery.removeBeer(biermenge);
+		}
+	}
+	public boolean isSellable() //Überprüft ob etwas gekauft werden darf
+	{
+		if (sellOrderBier == 0)
+		{
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public void executeEvent()
+	{
+		
+	}
+	private void sell()
+	{
+		bank.payIn(sellOrderBier*sellOrderPrice*checkMarket());
+		sellOrderBier = 0;
+		sellOrderPrice = 0;
+	}
+	private int checkMarket() //Überprüft, weiviel der Menge zum gesetzten Preis verkauft werden kan
+	{
+		return 1;
 	}
 }
