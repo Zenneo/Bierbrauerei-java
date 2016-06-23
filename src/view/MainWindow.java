d9a95d1e6d29060c81d0762158ca9f9ddf2ad7c8
chunkleader <chunkleader@gmail.com>
2016-06-23 11:12:26 +0200

    upgrade dialog can be opened;

--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -1,40 +1,13 @@
 package view;
 
-import org.eclipse.swt.widgets.Decorations;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Scale;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.FormLayout;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.widgets.Spinner;
-import org.eclipse.swt.layout.RowLayout;
-import org.eclipse.swt.widgets.Table;
-import swing2swt.layout.BoxLayout;
+import org.eclipse.swt.*;
+import org.eclipse.swt.widgets.*;
+import org.eclipse.swt.layout.*;
+import org.eclipse.swt.custom.*;
+import org.eclipse.swt.events.*;
+import org.eclipse.swt.graphics.*;
+
 import swing2swt.layout.FlowLayout;
-import org.eclipse.swt.widgets.DateTime;
-import org.eclipse.swt.custom.ViewForm;
-import org.eclipse.swt.widgets.Combo;
-import org.eclipse.swt.custom.StackLayout;
-import org.eclipse.swt.custom.SashForm;
-import org.eclipse.swt.widgets.Group;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.wb.swt.SWTResourceManager;
-import org.eclipse.swt.custom.ScrolledComposite;
-import org.eclipse.swt.custom.CLabel;
 
 public class MainWindow {
 
@@ -42,6 +15,11 @@ public class MainWindow {
 	private Text txtAdfasdfAsdfAsdf;
 	private Text eventDescription;
 
+	// constructor
+	public MainWindow() {
+		
+	}
+	
 	/**
 	 * Launch the application.
 	 * 
@@ -82,15 +60,15 @@ public class MainWindow {
 		shell.setSize(509, 401);
 		shell.setText("SWT Application");
 		shell.setLayout(new FillLayout(SWT.HORIZONTAL));
-		
+
 		Composite GameView = new Composite(shell, SWT.NONE);
 		GameView.setLayout(new FillLayout(SWT.HORIZONTAL));
-		
+
 		SashForm sashForm = new SashForm(GameView, SWT.VERTICAL);
-		
+
 		Composite Head = new Composite(sashForm, SWT.NONE);
 		Head.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
+
 		Button buttonNextRound = new Button(Head, SWT.CENTER);
 		buttonNextRound.addSelectionListener(new SelectionAdapter() {
 			@Override
@@ -98,92 +76,122 @@ public class MainWindow {
 			}
 		});
 		buttonNextRound.setText("Zum n\u00E4chster Tag");
-		
+
 		Composite Top = new Composite(sashForm, SWT.NONE);
 		Top.setLayout(new FillLayout(SWT.HORIZONTAL));
-		
+
 		Group TopLeft = new Group(Top, SWT.NONE);
 		TopLeft.setText("Markt");
 		TopLeft.setLayout(new FillLayout(SWT.VERTICAL));
-		
+
 		SashForm sashForm_2 = new SashForm(TopLeft, SWT.VERTICAL);
-		
+
 		Label beerPrice = new Label(sashForm_2, SWT.NONE);
 		beerPrice.setText("Bierpreis");
-		
+
 		SashForm price = new SashForm(sashForm_2, SWT.NONE);
-		
+
 		Scale price_scale = new Scale(price, SWT.NONE);
-		
+
 		Spinner price_spinner = new Spinner(price, SWT.BORDER);
-		price.setWeights(new int[] {1, 1});
-		
+		price.setWeights(new int[] { 1, 1 });
+
 		Label beerAmount = new Label(sashForm_2, SWT.NONE);
 		beerAmount.setText("Biermenge");
-		
+
 		SashForm amount = new SashForm(sashForm_2, SWT.NONE);
-		
+
 		Scale amount_scale = new Scale(amount, SWT.NONE);
-		
+
 		Spinner amount_spinner = new Spinner(amount, SWT.BORDER);
-		amount.setWeights(new int[] {2, 2, 4, 1});
-		sashForm_2.setWeights(new int[] {1, 2, 1, 2});
-		
+		sashForm_2.setWeights(new int[] { 1, 2, 1, 2 });
+
 		Group TopRight = new Group(Top, SWT.NONE);
 		TopRight.setText("B\u00FCro");
 		TopRight.setLayout(new FillLayout(SWT.HORIZONTAL));
-		
+
 		SashForm sashForm_1 = new SashForm(TopRight, SWT.VERTICAL);
-		
+
 		Label konto = new Label(sashForm_1, SWT.NONE);
 		konto.setText("Konto: xyz");
-		
+
 		Label kosten = new Label(sashForm_1, SWT.NONE);
 		kosten.setText("Kosten: xyz");
-		
+
 		Group grpAuftrage = new Group(sashForm_1, SWT.NONE);
 		grpAuftrage.setText("Auftr\u00E4ge");
 		grpAuftrage.setLayout(new FillLayout(SWT.VERTICAL));
-		
+
 		SashForm sashForm_3 = new SashForm(grpAuftrage, SWT.VERTICAL);
-		
+
 		Label eventTitle = new Label(sashForm_3, SWT.NONE);
-		eventTitle.setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.BOLD));
 		eventTitle.setText("eventTitle");
-		
-		eventDescription = new Text(sashForm_3, SWT.BORDER | SWT.READ_ONLY | SWT.WRAP | SWT.V_SCROLL | SWT.MULTI);
-		eventDescription.setText("afadf  asdf asdff asddaf aa sdf sdf sadf sdaf asdf asdjkfjskldj l klsjf lksjfkl jkl\u00F6jfkl\u00F6j\u00F6jiojao fjopij opjp oiaetuiop hz uioh opiuo  a");
-		sashForm_3.setWeights(new int[] {1, 5});
-		sashForm_1.setWeights(new int[] {1, 1, 7});
-		
+
+		eventDescription = new Text(sashForm_3, SWT.BORDER | SWT.READ_ONLY
+				| SWT.WRAP | SWT.V_SCROLL | SWT.MULTI);
+		eventDescription
+				.setText("afadf  asdf asdff asddaf aa sdf sdf sadf sdaf asdf asdjkfjskldj l klsjf lksjfkl jkl\u00F6jfkl\u00F6j\u00F6jiojao fjopij opjp oiaetuiop hz uioh opiuo  a");
+		sashForm_3.setWeights(new int[] { 1, 5 });
+		sashForm_1.setWeights(new int[] { 1, 1, 7 });
+
 		Composite Bottom = new Composite(sashForm, SWT.NONE);
 		Bottom.setLayout(new FillLayout(SWT.HORIZONTAL));
-		
+
 		Group BottomLeft = new Group(Bottom, SWT.NONE);
 		BottomLeft.setText("Brauerei");
-		
+
 		Label capacity = new Label(BottomLeft, SWT.NONE);
 		capacity.setBounds(10, 25, 55, 15);
 		capacity.setText("Kapazit\u00E4t");
-		
+
 		Label storage = new Label(BottomLeft, SWT.NONE);
 		storage.setBounds(10, 46, 55, 15);
 		storage.setText("Lager");
-		
+
 		Group BottomRight = new Group(Bottom, SWT.NONE);
 		BottomRight.setText("Upgrade");
 		BottomRight.setLayout(new FillLayout(SWT.HORIZONTAL));
-		
-		Button LagerButton = new Button(BottomRight, SWT.NONE);
-		LagerButton.addSelectionListener(new SelectionAdapter() {
+
+		Button storageButton = new Button(BottomRight, SWT.NONE);
+		storageButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 			}
 		});
-		LagerButton.setText("Lager");
-		
-		Button btnNewButton_1 = new Button(BottomRight, SWT.NONE);
-		btnNewButton_1.setText("Produktion");
-		sashForm.setWeights(new int[] {2, 5, 5});
+		storageButton.setText("Lager");
+
+		Button upgradeProduction = new Button(BottomRight, SWT.NONE);
+		upgradeProduction.setText("Produktion");
+		sashForm.setWeights(new int[] { 2, 5, 5 });
+
+		upgradeProduction.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event e) {
+				switch (e.type) {
+				case SWT.Selection:
+					
+					// Loop through all active shells and check if 
+			        // the shell is already open
+			        Shell[] shells = Display.getCurrent().getShells();
+
+			        for(Shell shell : shells) {
+			            String data = (String) shell.getData();
+
+			            // only activate the shell and return
+			            if(data != null && data.equals("yourID")) {
+			                shell.setFocus();
+			                return;
+			            }
+			        }
+
+			        // open the shell and the dialog
+			        Shell shell = new Shell(Display.getCurrent());
+			        shell.setData("yourID");
+			        upgradeStorage upgradestorage = new upgradeStorage(shell, 0);
+			        upgradestorage.open();
+				}
+			}
+
+		});
+
 	}
 }
--- src/view/upgradeStorage.java
+++ src/view/upgradeStorage.java
@@ -6,12 +6,12 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.SWT;
+import org.eclipse.swt.*;
 import org.eclipse.swt.custom.SashForm;
-import org.eclipse.swt.custom.ViewForm;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.events.*;
 
 public class upgradeStorage extends Dialog {
 
@@ -20,6 +20,7 @@ public class upgradeStorage extends Dialog {
 
 	/**
 	 * Create the dialog.
+	 * 
 	 * @param parent
 	 * @param style
 	 */
@@ -30,6 +31,7 @@ public class upgradeStorage extends Dialog {
 
 	/**
 	 * Open the dialog.
+	 * 
 	 * @return the result
 	 */
 	public Object open() {
@@ -49,61 +51,62 @@ public class upgradeStorage extends Dialog {
 	 * Create contents of the dialog.
 	 */
 	private void createContents() {
-		shlLagerupgrades = new Shell(getParent(), getStyle());
+		shlLagerupgrades = new Shell(getParent(), SWT.DIALOG_TRIM
+				| SWT.SYSTEM_MODAL);
 		shlLagerupgrades.setMinimumSize(new Point(100, 100));
 		shlLagerupgrades.setSize(344, 182);
 		shlLagerupgrades.setText("Lager-Upgrades");
 		shlLagerupgrades.setLayout(new FillLayout(SWT.VERTICAL));
-		
+
 		SashForm sashForm = new SashForm(shlLagerupgrades, SWT.VERTICAL);
-		
+
 		SashForm sashForm_2 = new SashForm(sashForm, SWT.NONE);
-		
+
 		Label lblNewLabel = new Label(sashForm_2, SWT.NONE);
 		lblNewLabel.setText("W\u00E4hle ein Upgrade!");
-		
+
 		Label lblNewLabel_1 = new Label(sashForm_2, SWT.NONE);
 		lblNewLabel_1.setAlignment(SWT.RIGHT);
 		lblNewLabel_1.setText("Aktuelles Kapital: 1002\u20AC");
-		sashForm_2.setWeights(new int[] {1, 1});
-		
+		sashForm_2.setWeights(new int[] { 1, 1 });
+
 		Group grpUpgrades = new Group(sashForm, SWT.NONE);
 		grpUpgrades.setText("Upgrades");
 		grpUpgrades.setLayout(new FillLayout(SWT.HORIZONTAL));
-		
+
 		SashForm sashForm_1 = new SashForm(grpUpgrades, SWT.NONE);
-		
+
 		SashForm sashForm_3 = new SashForm(sashForm_1, SWT.VERTICAL);
-		
+
 		Button upgrade_small = new Button(sashForm_3, SWT.NONE);
 		upgrade_small.setToolTipText("Beschreibung klein");
 		upgrade_small.setText("Klein");
-		
+
 		Label cost_small = new Label(sashForm_3, SWT.NONE);
 		cost_small.setText("69\u20AC");
-		sashForm_3.setWeights(new int[] {5, 1});
-		
+		sashForm_3.setWeights(new int[] { 5, 1 });
+
 		SashForm sashForm_4 = new SashForm(sashForm_1, SWT.VERTICAL);
-		
+
 		Button upgrade_medium = new Button(sashForm_4, SWT.NONE);
 		upgrade_medium.setToolTipText("Beschreibung mittel");
 		upgrade_medium.setText("Medium");
-		
+
 		Label cost_medium = new Label(sashForm_4, SWT.NONE);
 		cost_medium.setText("100\u20AC");
-		sashForm_4.setWeights(new int[] {5, 1});
-		
+		sashForm_4.setWeights(new int[] { 5, 1 });
+
 		SashForm sashForm_5 = new SashForm(sashForm_1, SWT.VERTICAL);
-		
+
 		Button upgrade_big = new Button(sashForm_5, SWT.NONE);
 		upgrade_big.setToolTipText("Beschreibung gro\u00DF");
 		upgrade_big.setText("Gro\u00DF");
-		
+
 		Label cost_big = new Label(sashForm_5, SWT.NONE);
 		cost_big.setText("10000000\u20AC");
-		sashForm_5.setWeights(new int[] {5, 1});
-		sashForm_1.setWeights(new int[] {1, 1, 1});
-		sashForm.setWeights(new int[] {2, 5});
+		sashForm_5.setWeights(new int[] { 5, 1 });
+		sashForm_1.setWeights(new int[] { 1, 1, 1 });
+		sashForm.setWeights(new int[] { 2, 5 });
 
 	}
 
