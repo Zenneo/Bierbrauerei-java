3a7a386b7438130630e26a379ebcc1df04505df1
PierreGames <pierre.lindenberg@gmail.com>
2016-06-23 11:10:19 +0200

    implementes the database

--- src/model/Event.java
+++ src/model/Event.java
@@ -1,15 +1,54 @@
 package model;
 
+import java.sql.*;
+
 public class Event {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public ReturnEvent getEvent(int round) {
+		int eventAmount = 0;
+		int eventPrice = 0;
+		int eventDurability = 0;
+		String eventName = "";
+		String eventDescription = "";
 
+		Connection c = null;
+		Statement stmt = null;
+		try {
+
+			// connect to db
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:.db");
+			c.setAutoCommit(false);
+			System.out.println("Opened database successfully");
+
+			stmt = c.createStatement();
+
+			// sql query
+			ResultSet rs = stmt.executeQuery("SELECT * FROM event WHERE minRound < " + round + " AND maxRound > " + round + ";");
+			ResultSetMetaData metaData = rs.getMetaData();
+			int rowcount = metaData.getColumnCount();
+			int random = (int) Math.floor(Math.random() * rowcount);
+			while (rs.next()) {
+				if (random <= 0) {
+					eventAmount = rs.getInt("amountFactor") * round
+							+ rs.getInt("amountOffset");
+					eventPrice = rs.getInt("pricePerBeer") * eventAmount;
+					eventDurability = rs.getInt("durability");
+					eventName = rs.getString("name");
+					eventDescription = rs.getString("description");
+				} else {
+					random--;
+				}
+			}
+			rs.close();
+
+			// close connection
+			stmt.close();
+			c.close();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		return new ReturnEvent(eventName, eventDescription, eventPrice, eventAmount, eventDurability);
 	}
-    public ReturnEvent getEvent(int round)
-    {
-    	ReturnEvent currentEvent = new ReturnEvent("DemoEvent", "Zu Demozwecken fordert dieser Auftrag von dir 100 Fäßer Bier.", 2500, 100, 5);
-		return currentEvent;
-    	
-    }
 }
