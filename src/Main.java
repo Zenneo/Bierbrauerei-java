f245507dab78b47648adc23794558ece031ec662
Zenneo <agrar.pfurtz@gmail.com>
2016-07-07 10:55:21 +0200

    Button functionality

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 import model.*;
 import view.*;
 
-
 public class Main {
 	public static void main(String args[]) {
 		RoundManager roundmanager = new RoundManager();
-		
+
 		// create main window
 		MainWindow mainWindow = new MainWindow(roundmanager);
 		mainWindow.open();
-		roundmanager.getNewEvent();
+
 	}
 }
